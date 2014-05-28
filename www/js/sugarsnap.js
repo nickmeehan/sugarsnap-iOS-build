ecddec784b49b466f255c90bacabc85fd3e96918
Apprentice <apprentice@dbc30.local>
2014-05-28 10:58:53 -0700

    built

--- www/js/sugarsnap.js
+++ www/js/sugarsnap.js
@@ -3,18 +3,11 @@ SugarSnap = {
 		navigator.geolocation.getCurrentPosition(this.getCoordinatesSuccess, this.getCoordinatesFailure)
 	},
 	getCoordinatesSuccess: function(position) {
-		// start spinner here
-		alert('position.coords.latitude')
-		// console.log(position)
 		var firebaseController = new FirebaseController(position, FirebaseConnection.getGeo())
-		alert(firebaseController.latitude)
 		var photoController = new PhotoController(new PhotoView())
 		firebaseController.subscribeListenerForInitialPhotos(photoController)
 		firebaseController.subscribeListenerForLivePhotoUpdates(photoController)
-
-		// infinite scroll needs to be added
 		InfiniteScroller.checkScrollThreshold(photoController)
-
 		new CameraController(new CameraView()).bindCameraListener()
 	},
 	getCoordinatesFailure: function() {
